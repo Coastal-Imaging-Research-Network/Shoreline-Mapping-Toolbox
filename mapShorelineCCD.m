fe99580e2cb0ee5a4c7e8428fe992abb53c98a30
mitchharley <m.harley@unsw.edu.au>
2019-06-18 05:53:02 +1000

    Added plotoption so that users dont need to plot results

--- mapShorelineCCD.m
+++ mapShorelineCCD.m
@@ -1,4 +1,4 @@
-function sl = mapShorelineCCD(xgrid,ygrid,Iplan,transects,editoption)
+function sl = mapShorelineCCD(xgrid,ygrid,Iplan,transects,plotoption,editoption)
 %
 %function sl = mapShorelineCCD(xgrid,ygrid,Iplan,transects,editoption)
 %
@@ -11,24 +11,29 @@ function sl = mapShorelineCCD(xgrid,ygrid,Iplan,transects,editoption)
 %Iplan -- a MxNx3 uint8 image of the planview
 %transects -- a structure that seeds the shoreline search algorithm
 %using a series of cross-shore transects spaced at ~5m apart
-   % transects.x -- the start and end points of each transect
-   % x-coordinates (2 x M matrix, where 1st row = start, 2nd row = end)
-   % transects.y -- the start and end points of the transects
-   % y-coordinates (2 x M matrix, where 1st row = start, 2nd row = end)
+% transects.x -- the start and end points of each transect
+% x-coordinates (2 x M matrix, where 1st row = start, 2nd row = end)
+% transects.y -- the start and end points of the transects
+% y-coordinates (2 x M matrix, where 1st row = start, 2nd row = end)
 %editoption == 1 if you want to manually edit the shoreline (using
 %               interactive point editor)
 %
 %Created by Mitch Harley
 %June 2018
 
-   
+
 if nargin==4
+    plotoption=1;
     editoption=1;
+elseif nargin==5
+    editoption=0;
 end
 
-f1=figure;
-image(xgrid,ygrid,Iplan)
-axis image; axis xy
+if plotoption==1
+    f1=figure;
+    image(xgrid,ygrid,Iplan)
+    axis image; axis xy
+end
 [X,Y] = meshgrid(xgrid,ygrid);
 
 %Find threshold
@@ -38,23 +43,24 @@ xlabel_type = 'Red minus blue';
 thresh_weightings = [1/3 2/3]; %This weights the authomatic thresholding towards the sand peak (works well in SE Australia)
 %[peak_values,peak_locations]=findpeaks(pdf_values,pdf_locs); %Find peaks
 thresh_otsu = multithresh(P(:,:,1)-P(:,:,3)); %Threshold using Otsu's method
-f2 = figure;
-plot(pdf_locs,pdf_values)
-hold on
 I1 = find(pdf_locs<thresh_otsu);
 [~,J1] = max(pdf_values(I1));
 I2 = find(pdf_locs>thresh_otsu);
 [~,J2] = max(pdf_values(I2));
-plot(pdf_locs([I1(J1) I2(J2)]),pdf_values([I1(J1) I2(J2)]),'ro')
-%thresh = mean(peak_locations([I1(J1) I2(J2)])); %only find the last two peaks
 thresh = thresh_weightings(1)*pdf_locs(I1(J1)) + thresh_weightings(2)*pdf_locs(I2(J2)); %Skew average towards the positive (i.e. sand pixels)
-
-YL = ylim;
-plot([thresh thresh], YL,'r:','linewidth',2)
-%plot([thresh_otsu thresh_otsu], YL,'g:','linewidth',2)
-xlabel(xlabel_type,'fontsize',10)
-ylabel('Counts','fontsize',10)
 disp(['Sand/water threshold determined to be ' num2str(thresh, '%0.0f')])
+
+if plotoption==1
+    f2 = figure;
+    plot(pdf_locs,pdf_values)
+    hold on
+    plot(pdf_locs([I1(J1) I2(J2)]),pdf_values([I1(J1) I2(J2)]),'ro')
+    YL = ylim;
+    plot([thresh thresh], YL,'r:','linewidth',2)
+    %plot([thresh_otsu thresh_otsu], YL,'g:','linewidth',2)
+    xlabel(xlabel_type,'fontsize',10)
+    ylabel('Counts','fontsize',10)
+end
 
 %Extract contour
 RminusBdouble = double(Iplan(:,:,1))- double(Iplan(:,:,3));
@@ -67,7 +73,7 @@ RminusBdouble(Imask) = NaN; %Mask data
 %Imask = find(RminusBdouble==0); %Also remove regions of black colour
 %RminusBdouble(Imask) = NaN; %Mask data
 c = contours(X,Y,RminusBdouble,[thresh thresh]);
-figure(f1)
+
 
 %Now look at contours to only find the longest contour (assumed to be the
 %shoreline)
@@ -75,7 +81,7 @@ II = find(c(1,:)==thresh);
 if II==1 %If only one line
     startI = 2;
     endI = size(c,2);
-else   
+else
     D = diff(II);
     [~,J] = max(D); %Select contour that is the longest continuous contour
     if J == 1
@@ -97,16 +103,17 @@ for i = 1:length(transects.x)
     angle = atan(diff(transects.y(:,i))/diff(transects.x(:,i)));
     points_rot = rotatePoints(points,angle,[transects.x(1,i) transects.y(1,i)],'rads');
     max_distance = sqrt(diff(transects.y(:,i))^2+ diff(transects.x(:,i))^2);
-        I = find(points_rot(:,2)>-1&points_rot(:,2)<1&points_rot(:,1)>0&points_rot(:,1)<max_distance); %Only find points greater than zero so that they are in the roi
-        if ~isempty(I)
-            [~,Imin] = min(points_rot(I,1));
-            %[~,Imin] = max(points_rot(I,1));
-            sl.x(i) = points(I(Imin),1);
-            sl.y(i) = points(I(Imin),2);
-        end
+    I = find(points_rot(:,2)>-1&points_rot(:,2)<1&points_rot(:,1)>0&points_rot(:,1)<max_distance); %Only find points greater than zero so that they are in the roi
+    if ~isempty(I)
+        [~,Imin] = min(points_rot(I,1));
+        %[~,Imin] = max(points_rot(I,1));
+        sl.x(i) = points(I(Imin),1);
+        sl.y(i) = points(I(Imin),2);
+    end
 end
 
 if editoption ==1
+    figure(f1)
     h = impoly(gca,[sl.x' sl.y'],'closed',0);
     disp('Please edit your shoreline now or press any key to continue')
     pause
